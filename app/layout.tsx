e1cf7011f568f47bd2b78168afb90ecfbdc8e50e
kris1027 <krzys89@gmail.com>
2025-06-12 16:59:48 +0200

    feat: implement NavigationBar component with navigation links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import NavigationBar from '@/components/navigation/navigation-bar';
 
 export const metadata: Metadata = {
   title: 'workoutXP',
@@ -14,9 +15,10 @@ const RootLayout = ({
   return (
     <html lang='en'>
       <body className='min-h-screen flex justify-center items-center'>
-        <main className='relative z-10 w-96 max-w-full min-h-[667px] p-4 bg-white text-black rounded-3xl overflow-hidden flex flex-col backdrop-blur-sm shadow-[0_8px_32px_0_rgba(255,255,255,0.25),0_20px_60px_0_rgba(139,69,255,0.3),0_0_0_1px_rgba(255,255,255,0.1)]'>
-          {children}
-        </main>
+        <div className='relative z-10 w-96 max-w-full min-h-[667px] p-4 bg-white text-black rounded-3xl overflow-hidden flex flex-col justify-between backdrop-blur-sm shadow-[0_8px_32px_0_rgba(255,255,255,0.25),0_20px_60px_0_rgba(139,69,255,0.3),0_0_0_1px_rgba(255,255,255,0.1)]'>
+          <main>{children}</main>
+          <NavigationBar />
+        </div>
       </body>
     </html>
   );
--- components/navigation/navigation-bar.tsx
+++ components/navigation/navigation-bar.tsx
@@ -1,5 +1,24 @@
+import Link from 'next/link';
+
+const navLinks = [
+  { name: 'Home', href: '/' },
+  { name: 'Exercises', href: 'exercises' },
+  { name: 'Workouts', href: 'workouts' },
+  { name: 'Profile', href: 'profile' },
+];
+
 const NavigationBar = () => {
-  return <div>NavigationBar</div>;
+  return (
+    <nav>
+      <ul className='flex justify-around items-center bg-gray-800 text-white p-4 rounded-lg'>
+        {navLinks.map((link) => (
+          <li key={link.name}>
+            <Link href={link.href}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavigationBar;
