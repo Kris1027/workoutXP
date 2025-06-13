b31fe3160ca6f107e44bb1a22bf0e2ffc7eaa284
kris1027 <krzys89@gmail.com>
2025-06-13 08:04:45 +0200

    feat: integrate next-view-transitions for improved navigation and update NavigationBar import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from 'next';
-import './globals.css';
 import NavigationBar from '@/components/navigation/navigation-bar';
+import type { Metadata } from 'next';
+import { ViewTransitions } from 'next-view-transitions';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'workoutXP',
@@ -13,14 +14,16 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <html lang='en'>
-      <body className='min-h-screen flex justify-center items-center'>
-        <div className='relative z-10 w-[390px] max-w-full min-h-[844px] bg-white text-black rounded-3xl overflow-hidden flex flex-col justify-between backdrop-blur-sm shadow-[0_8px_32px_0_rgba(255,255,255,0.25),0_20px_60px_0_rgba(139,69,255,0.3),0_0_0_1px_rgba(255,255,255,0.1)]'>
-          <main>{children}</main>
-          <NavigationBar />
-        </div>
-      </body>
-    </html>
+    <ViewTransitions>
+      <html lang='en'>
+        <body className='min-h-screen flex justify-center items-center'>
+          <div className='relative z-10 w-[390px] max-w-full min-h-[844px] bg-white text-black rounded-3xl overflow-hidden flex flex-col justify-between backdrop-blur-sm shadow-[0_8px_32px_0_rgba(255,255,255,0.25),0_20px_60px_0_rgba(139,69,255,0.3),0_0_0_1px_rgba(255,255,255,0.1)]'>
+            <main>{children}</main>
+            <NavigationBar />
+          </div>
+        </body>
+      </html>
+    </ViewTransitions>
   );
 };
 
