c3a1f3722f8b049a345b7fb7ea2a4c513c544885
kris1027 <krzys89@gmail.com>
2025-06-13 07:28:48 +0200

    feat: add active subpage different color on the navigation bar

--- components/navigation/navigation-bar.tsx
+++ components/navigation/navigation-bar.tsx
@@ -1,18 +1,24 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const navLinks = [
   { name: 'Home', href: '/' },
-  { name: 'Exercises', href: 'exercises' },
-  { name: 'Workouts', href: 'workouts' },
-  { name: 'Profile', href: 'profile' },
+  { name: 'Exercises', href: '/exercises' },
+  { name: 'Workouts', href: '/workouts' },
+  { name: 'Profile', href: '/profile' },
 ];
 
 const NavigationBar = () => {
+  const pathname = usePathname();
+  const isActive = (href: string) => pathname === href;
+
   return (
     <nav>
       <ul className='flex justify-around items-center bg-gray-800 text-white p-4 rounded-lg'>
         {navLinks.map((link) => (
-          <li key={link.name}>
+          <li key={link.name} className={isActive(link.href) ? 'text-violet-400' : ''}>
             <Link href={link.href}>{link.name}</Link>
           </li>
         ))}
