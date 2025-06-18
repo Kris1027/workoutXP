bd86a35e8d620a5d15f929abd894063d6c84609f
kris1027 <krzys89@gmail.com>
2025-06-18 15:18:46 +0200

    feat: update ExerciseProps type to use string for category and difficulty for flexibility

--- types/data-types.ts
+++ types/data-types.ts
@@ -1,11 +1,8 @@
-type ExerciseCategory = 'Chest' | 'Shoulders';
-type ExerciseDifficulty = 'Beginner' | 'Intermediate' | 'Advanced';
-
 export type ExerciseProps = {
   id?: string;
   name: string;
-  category: ExerciseCategory;
-  difficulty: ExerciseDifficulty;
+  category: string;
+  difficulty: string;
   ImageUrl: string;
   description: string;
   createdAt?: Date;
