17d818ec147ff2729d231ddebed5229e9c1b5b91
kris1027 <krzys89@gmail.com>
2025-06-24 15:58:19 +0200

    feat: add workout creation form to WorkoutsPage component

--- app/workouts/page.tsx
+++ app/workouts/page.tsx
@@ -1,5 +1,7 @@
 import WorkoutList from '@/components/workouts/workout-list';
 import prisma from '@/lib/prisma';
+import { revalidatePath } from 'next/cache';
+import Form from 'next/form';
 
 const WorkoutsPage = async () => {
   const workouts = await prisma.workout.findMany({
@@ -9,7 +11,41 @@ const WorkoutsPage = async () => {
     },
   });
 
-  return <WorkoutList workouts={workouts} />;
+  const createWorkout = async (formData: FormData) => {
+    'use server';
+    const name = formData.get('name') as string;
+    const imageUrl =
+      'https://fra.cloud.appwrite.io/v1/storage/buckets/exercise-images-storage/files/682ba1580028ac3bbc20/view?project=gym-app&mode=admin';
+    const description = formData.get('description') as string;
+
+    await prisma.workout.create({
+      data: {
+        name,
+        description,
+        imageUrl,
+      },
+    });
+    revalidatePath('/workouts');
+  };
+
+  return (
+    <>
+      <Form action={createWorkout} className='p-10'>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <input type='text' id='name' name='name' required />
+        </div>
+        <div>
+          <label htmlFor='description'>Description</label>
+          <textarea id='description' name='description' required></textarea>
+        </div>
+        <button type='submit' className='bg-blue-500 text-white px-4 py-2 rounded'>
+          Create Workout
+        </button>
+      </Form>
+      <WorkoutList workouts={workouts} />;
+    </>
+  );
 };
 
 export default WorkoutsPage;
