04e53d16a6f0d8146d8f231cdffa5bf33d148563
kris1027 <krzys89@gmail.com>
2025-07-03 15:11:29 +0200

    refactor: improve error handling in workout actions

--- actions/workouts-action.ts
+++ actions/workouts-action.ts
@@ -5,42 +5,55 @@ import type { WorkoutProps } from '@/types/data-types';
 import { revalidatePath } from 'next/cache';
 
 export const fetchWorkouts = async (): Promise<WorkoutProps[]> => {
-  return prisma.workout.findMany({
-    orderBy: { createdAt: 'desc' },
-    include: {
-      exercises: true,
-    },
-  });
+  try {
+    return prisma.workout.findMany({
+      orderBy: { createdAt: 'desc' },
+      include: {
+        exercises: true,
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching workouts', error);
+    throw new Error(error instanceof Error ? error.message : 'Unexpected error');
+  }
 };
 
 export const fetchWorkoutById = async (id: string): Promise<WorkoutProps | null> => {
-  return prisma.workout.findUnique({
-    where: { id: id },
-    include: {
-      exercises: true,
-    },
-  });
+  try {
+    return prisma.workout.findUnique({
+      where: { id: id },
+      include: {
+        exercises: true,
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching workout', error);
+    throw new Error(error instanceof Error ? error.message : 'Unexpected error');
+  }
 };
 
 export const createWorkout = async (workoutData: WorkoutProps): Promise<void> => {
   const { name, description, imageUrl, exercises } = workoutData;
 
-  // Get selected exercise IDs
   const selectedExerciseIds = exercises.map((exercise) => exercise.id);
 
-  // Create workout with selected exercises
-  await prisma.workout.create({
-    data: {
-      name,
-      description,
-      imageUrl,
-      exercises: {
-        connect: selectedExerciseIds.map((id) => ({ id })),
+  try {
+    await prisma.workout.create({
+      data: {
+        name,
+        description,
+        imageUrl,
+        exercises: {
+          connect: selectedExerciseIds.map((id) => ({ id })),
+        },
       },
-    },
-  });
+    });
 
-  revalidatePath('/workouts');
+    revalidatePath('/workouts');
+  } catch (error) {
+    console.error('Error creating workout:', error);
+    throw new Error(error instanceof Error ? error.message : 'Unexpected error');
+  }
 };
 
 export const deleteWorkout = async (workoutId: string): Promise<void> => {
