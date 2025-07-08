7bb7c29f6b2d5f08a082b5f2692192aa5ac663ea
kris1027 <krzys89@gmail.com>
2025-07-08 10:36:59 +0200

    feat: update exercise images in seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,8 +8,7 @@ const exerciseData: ExerciseProps[] = [
     name: 'Incline Chest Fly',
     category: 'Chest',
     difficulty: 'Intermediate',
-    imageUrl:
-      'https://fra.cloud.appwrite.io/v1/storage/buckets/exercise-images-storage/files/682b9cdc0023420f850c/view?project=gym-app&mode=admin',
+    imageUrl: 'https://szqmv3habx.ufs.sh/f/FYhVXcwcWhNICkwomn0hM3lKBTWRXHASGJez5yw9Eda0gOhf',
     description:
       'An exercise that targets the upper chest muscles by performing a fly motion on an incline bench.',
   },
@@ -17,8 +16,7 @@ const exerciseData: ExerciseProps[] = [
     name: 'Incline Dumbbell Press',
     category: 'Chest',
     difficulty: 'Beginner',
-    imageUrl:
-      'https://fra.cloud.appwrite.io/v1/storage/buckets/exercise-images-storage/files/682b9fc900131446e9a5/view?project=gym-app&mode=admin',
+    imageUrl: 'https://szqmv3habx.ufs.sh/f/FYhVXcwcWhNICkwomn0hM3lKBTWRXHASGJez5yw9Eda0gOhf',
     description:
       'An exercise that targets the shoulders by pressing dumbbells overhead on an incline bench.',
   },
@@ -26,8 +24,7 @@ const exerciseData: ExerciseProps[] = [
     name: 'Butterfly Machine',
     category: 'Chest',
     difficulty: 'Beginner',
-    imageUrl:
-      'https://fra.cloud.appwrite.io/v1/storage/buckets/exercise-images-storage/files/682b9e860039f0f5c82f/view?project=gym-app&mode=admin',
+    imageUrl: 'https://szqmv3habx.ufs.sh/f/FYhVXcwcWhNICkwomn0hM3lKBTWRXHASGJez5yw9Eda0gOhf',
     description:
       'A machine-based exercise that targets the chest muscles by performing a butterfly motion.',
   },
@@ -35,8 +32,7 @@ const exerciseData: ExerciseProps[] = [
     name: 'Seated Overhead Dumbbell Press',
     category: 'Shoulders',
     difficulty: 'Advanced',
-    imageUrl:
-      'https://fra.cloud.appwrite.io/v1/storage/buckets/exercise-images-storage/files/682ba1580028ac3bbc20/view?project=gym-app&mode=admin',
+    imageUrl: 'https://szqmv3habx.ufs.sh/f/FYhVXcwcWhNICkwomn0hM3lKBTWRXHASGJez5yw9Eda0gOhf',
     description:
       'An exercise that targets the shoulders by pressing dumbbells overhead while seated.',
   },
@@ -44,8 +40,7 @@ const exerciseData: ExerciseProps[] = [
     name: 'Rope Cable Triceps Extension',
     category: 'Shoulders',
     difficulty: 'Intermediate',
-    imageUrl:
-      'https://fra.cloud.appwrite.io/v1/storage/buckets/exercise-images-storage/files/682ba3a3001abb20648e/view?project=gym-app&mode=admin',
+    imageUrl: 'https://szqmv3habx.ufs.sh/f/FYhVXcwcWhNICkwomn0hM3lKBTWRXHASGJez5yw9Eda0gOhf',
     description:
       'An exercise that targets the triceps by extending a rope attachment on a cable machine overhead.',
   },
@@ -53,8 +48,7 @@ const exerciseData: ExerciseProps[] = [
     name: 'Bench Press',
     category: 'Chest',
     difficulty: 'Intermediate',
-    imageUrl:
-      'https://fra.cloud.appwrite.io/v1/storage/buckets/exercise-images-storage/files/682c36f300035fc51c05/view?project=gym-app&mode=admin',
+    imageUrl: 'https://szqmv3habx.ufs.sh/f/FYhVXcwcWhNICkwomn0hM3lKBTWRXHASGJez5yw9Eda0gOhf',
     description:
       'A compound exercise that targets the chest, shoulders, and triceps by pressing a barbell from the chest to an extended position.',
   },
@@ -62,8 +56,7 @@ const exerciseData: ExerciseProps[] = [
     name: 'Incline Bench Press',
     category: 'Chest',
     difficulty: 'Intermediate',
-    imageUrl:
-      'https://fra.cloud.appwrite.io/v1/storage/buckets/exercise-images-storage/files/682ba3ad000bc1f48ab2/view?project=gym-app&mode=admin',
+    imageUrl: 'https://szqmv3habx.ufs.sh/f/FYhVXcwcWhNICkwomn0hM3lKBTWRXHASGJez5yw9Eda0gOhf',
     description:
       'A variation of the bench press that targets the upper chest by pressing a barbell from an inclined position.',
   },
