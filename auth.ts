ba11266fc130a8a67e28b495101665dab6435489
kris1027 <krzys89@gmail.com>
2025-07-09 11:21:48 +0200

    feat: integrate Prisma adapter and update authentication models for user management

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import GitHub from 'next-auth/providers/github';
+import { prisma } from '@/lib/prisma';
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub],
 });
