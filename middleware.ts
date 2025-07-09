1ab30de4651beaa347bed27bc6473a75cadb81f5
kris1027 <krzys89@gmail.com>
2025-07-09 09:41:49 +0200

    feat: implement middleware for protected routes with authentication check

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from './auth';
+
+const protectedRoutes = ['/profile'];
+
+const middleware = async (req: NextRequest) => {
+  const session = await auth();
+
+  const { pathname } = req.nextUrl;
+
+  const isProtected = protectedRoutes.some((route) => pathname.startsWith(route));
+
+  if (isProtected && !session) {
+    return NextResponse.redirect(new URL('/api/auth/signin', req.url));
+  }
+
+  return NextResponse.next();
+};
+
+export default middleware;
