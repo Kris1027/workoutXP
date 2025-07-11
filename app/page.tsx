0b5bdd44a1844c9a57c22ff2f8a9b422217378cd
kris1027 <krzys89@gmail.com>
2025-07-11 16:28:03 +0200

    feat: add logo image in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import Image from 'next/image';
+
 const HomePage = async () => {
-  return <div>Homepage</div>;
+  const logoUrl = process.env.LOGO_URL || '';
+
+  return (
+    <div className='flex items-center justify-center p-4'>
+      <div className='relative w-full max-w-md md:max-w-lg lg:max-w-xl'>
+        <Image
+          src={logoUrl}
+          width={400}
+          height={400}
+          alt='workoutXP logo'
+          className='w-full h-auto object-contain'
+          priority
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
